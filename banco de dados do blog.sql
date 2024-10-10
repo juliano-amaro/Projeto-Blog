e009a1442eadc9a3907b81c689d7331ddf1919ce
Juliano Corrêa <165314925+juliano-amaro@users.noreply.github.com>
2024-10-10 15:36:42 -0300

    Add files via upload

---
+++ banco de dados do blog.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE blog;
+
+USE blog;
+
+CREATE TABLE usuario (
+
+	id_usuario INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(45) NOT NULL,
+    email VARCHAR(45) UNIQUE NOT NULL,
+    senha VARCHAR(45) NOT NULL
+
+); 
+
+CREATE TABLE post (
+
+	id_post INT PRIMARY KEY AUTO_INCREMENT,
+    id_usuario INT,
+    conteudo TEXT NOT NULL,
+    data_post DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
+
+);
+
+INSERT INTO usuario (nome, email, senha) VALUES
+('Paulo', 'paulo@email.com', 'paulo123');
+
+INSERT INTO usuario (nome, email, senha) VALUES
+('Pedro', 'pedro@email.com', 'pedro123');
+
+INSERT INTO usuario (nome, email, senha) VALUES
+('Josiane', 'josiane@email.com', 'josiane123'),
+('José', 'jose@email.com', 'jose123');
